0ac93255c3c954eb19f1a592c64aba85909254c5
rishikhurana17 <rishikhurana17@gmail.com>
2018-11-04 18:18:09 -0800

    Decorator Pattern

---
+++ DecoratorPattern/Pizza.java
@@ -0,0 +1,25 @@
+package DesignPatterns.DecoratorPattern;
+
+/**
+ * Created by rkhurana on 11/4/18.
+ */
+
+public interface Pizza {
+
+    public String getDescription();
+
+    public double getCost();
+
+}
+
+//public abstract class Pizza{
+//
+//
+//	public abstract void setDescription(String newDescription);
+//	public abstract String getDescription();
+//
+//	public abstract void setCost(double newCost);
+//	public abstract double getCost();
+//
+//
+//}
---
+++ DecoratorPattern/ThreeCheesePizza.java
@@ -0,0 +1,35 @@
+package DesignPatterns.DecoratorPattern;
+
+/**
+ * Created by rkhurana on 11/4/18.
+ */
+
+//public class ThreeCheesePizza extends Pizza{
+//
+//    private String description = "Mozzarella, Fontina, Parmesan Cheese Pizza";
+//    private double cost = 10.50;
+//
+//    public void setDescription(String newDescription) {
+//
+//        description = newDescription;
+//
+//    }
+//
+//    public String getDescription() {
+//
+//        return description;
+//
+//    }
+//
+//    public void setCost(double newCost) {
+//
+//        cost = newCost;
+//
+//    }
+//
+//    public double getCost() {
+//
+//        return cost;
+//
+//    }
+//}
